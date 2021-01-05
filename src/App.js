a095f0d3fefa80dbe5b93a81582a02e7f5e83d65
amonae <74229895+amonae@users.noreply.github.com>
2021-01-05 14:08:19 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Users, Home } from './components/Pages';
-
+import { PostProvider, PostContext } from './PostContext';
 import './styles.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/saved">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <PostProvider>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/saved">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </PostProvider>
   );
 }
---
+++ src/PostContext.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, createContext } from 'react';
+
+export const PostContext = createContext();
+
+export function PostProvider(props) {
+  let [posts, setPosts] = useState([]);
+  let [savedPosts, setSavedPosts] = useState([]);
+
+  useEffect(() => {
+    if (savedPosts && savedPosts.length > 0) {
+      localStorage.setItem('saved posts', JSON.stringify(savedPosts));
+    } else {
+      console.log(`There are no new saved posts (prior to refresh).`);
+    }
+  }, [savedPosts]);
+
+  useEffect(() => {
+    if (localStorage.getItem('saved posts')) {
+      setSavedPosts(JSON.parse(localStorage.getItem('saved posts')));
+    } else {
+      console.log('There is no local storage.');
+    }
+  }, []);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      setPosts(data);
+    };
+
+    getPosts();
+  }, []);
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: posts,
+        setPosts: setPosts,
+        savedPosts: savedPosts,
+        setSavedPosts: setSavedPosts
+      }}
+    >
+      {props.children}
+    </PostContext.Provider>
+  );
+}
