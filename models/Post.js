6f8dfb41291dc389f31531d325afecf9befed3e7
Ahniya Payne <ahniyap9231@gmail.com>
2021-01-14 17:11:01 -0600

    initial commit

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  htmlContent: {
+    type: String,
+    required: true,
+  },
+  deltaContent: {
+    type: String,
+    required: true,
+  },
+  dateCreated: {
+    type: String,
+    required: true,
+  },
+  //TODO: MAKE DATEMODIFIED REQUIRED. NEW POSTS' DATEMODIFIED WILL JUST BE EQUAL TO THEIR DATECREATED!!!
+  dateModified: {
+    type: String,
+    required: false,
+  },
+  //TODO: CREATE THE AUTHOR RELATIONSHIP
+  // author: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "User",
+  //   required: true
+  // }
+});
+
+module.exports = mongoose.model("Post", postSchema);
---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  //TODO: Create CREATEDPOSTS and SAVEDPOSTS
+});
+
+module.exports = mongoose.model("User", userSchema);
