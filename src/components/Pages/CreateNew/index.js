2e7b27e4d9f0126b38fb5012fc2f02d7a58f2c88
Ahniya Payne <ahniyap9231@gmail.com>
2021-01-30 02:51:41 -0600

    fix timelimitmode, create wordcountmode finally fixed the damn timer in timelimitmode, it was super buggy before!

--- src/components/Pages/CreateNew/index.js
+++ src/components/Pages/CreateNew/index.js
@@ -1,20 +1,9 @@
 import React, { useRef, useEffect, useState } from "react";
 
-import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-import {
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-} from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
-import { Stack, HStack } from "@chakra-ui/react";
-import { useToast } from "@chakra-ui/react";
-import { BiRefresh } from "react-icons/bi";
-
 import { Layout } from "../../Pages";
+import { TimeLimitMode } from "../../WritingModes/TimeLimitMode";
+import { PromptMode } from "../../WritingModes/PromptMode";
+import { WordCountMode } from "../../WritingModes/WordCountMode";
 
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
@@ -25,15 +14,36 @@ import ContentEditable from "react-contenteditable";
 export function CreateNew() {
   let quillEditor = useRef(null);
   let postTitle = useRef(null);
-  let [mode, setMode] = useState("");
+  const [mode, setMode] = useState("");
+  const [wordCountGoal, setWordCountGoal] = useState(null);
+  const [wordCount, setWordCount] = useState(0);
+  const [val, setVal] = useState(0);
+
   // useReducer
 
-  //! Modes should be put inside PostContext so that it doesn't wipe out everytime the component re-renders!!! It needs to be cached/remembered somehow!
+  //TODO: put all state from the different modes into this component. this will make it so that even if different modes are switched, their progress from that mode persists! (except for the counter... THAT will stop once the mode is changed to something else, so be sure to warn the user);
 
-  //? state that is an object with 3 keys: prompt mode, time mode, word count mode. their values are booleans. use this to decide which component will be rendered/mode will be activated?
+  //TODO: ^ create another Context for the modes (ModesContext)
+
+  function getWordCount() {
+    if (mode !== "wordCountMode") {
+      return;
+    }
+
+    let quillTextArea = quillEditor.current.getEditor().getText();
+
+    let words = quillTextArea.match(/\b[-?(\w+)?]+\b/gi);
+
+    if (words) {
+      setWordCount(words.length);
+    } else {
+      console.log("0 words");
+    }
+  }
+
+  //? notice: the mode states are persisting when their state is inside of the parent component (duh.) move it all up (or create new context)!
 
   function handleSave() {
-    // let postTitle = quillEditor.current.
     let textContents = quillEditor.current.state.value;
     let deltaContents = quillEditor.current.editor.getContents();
     let title = postTitle.current.el.current.innerText;
@@ -48,8 +58,11 @@ export function CreateNew() {
 
   return (
     <Layout>
-      <TextEditor postTitleRef={postTitle} quillEditorRef={quillEditor} />
-      {/* <button onClick={() => handleSave()}>Save</button> */}
+      <TextEditor
+        postTitleRef={postTitle}
+        getWordCount={() => getWordCount()}
+        quillEditorRef={quillEditor}
+      />
       <div className="modeSelection">
         <input
           type="radio"
@@ -82,101 +95,22 @@ export function CreateNew() {
         <br></br>
       </div>
 
-      {mode === "promptMode" && <PromptMode />}
-      {mode === "wordCountMode" && <WordCountMode />}
-      {mode === "timeLimitMode" && <TimeLimitMode />}
+      {mode === "promptMode" && <PromptMode mode={mode} />}
+      {mode === "wordCountMode" && (
+        <WordCountMode
+          wordCount={wordCount}
+          wordCountGoal={wordCountGoal}
+          setWordCountGoal={setWordCountGoal}
+          quillEditor={quillEditor}
+          mode={mode}
+        />
+      )}
+      {mode === "timeLimitMode" && <TimeLimitMode mode={mode} />}
     </Layout>
   );
 }
 
-function WordCountMode() {
-  return <p>This is the wordcountmode component.</p>;
-  // Implement a Chakra UI progress bar to indicate how far along user is to meeting their word count goal.
-}
-
-function TimeLimitMode() {
-  const [timeLimit, setTimeLimit] = useState(null);
-  const [val, setVal] = useState(null);
-  const [isReadOnly, setIsReadOnly] = useState(false);
-
-  // create context state for each mode. if a radio button is clicked/mode is activated, then set the state to whatever mode was activated. when user clicks on another mode, the state updates. do this so that i can effectively cleanup any processes happening in the current state
-
-  //! THE COUNTDOWN CONTINUES TO RUN EVEN AFTER SWITCHING TO ANOTHER MODE!
-
-  const toast = useToast();
-
-  useEffect(() => {
-    if (timeLimit === 0) {
-      toast({
-        title: "Time is up!",
-        duration: 10000,
-        status: "info",
-        isClosable: true,
-      });
-    }
-  }, [timeLimit]);
-
-  function setCountdown() {
-    let minutes = val * 60 + 1;
-    setIsReadOnly(true);
-
-    const countdown = setInterval(() => {
-      minutes -= 1;
-      setTimeLimit(minutes);
-      if (minutes <= 0) {
-        clearInterval(countdown);
-        setIsReadOnly(false);
-      }
-
-      console.log(minutes);
-    }, 1000);
-
-    return () => {
-      clearInterval(countdown);
-    };
-  }
-
-  return (
-    <>
-      <HStack spacing="10px">
-        <CircularProgress
-          value={timeLimit !== null ? timeLimit : 0}
-          min={0}
-          max={val !== 0 ? val * 60 : 1}
-          size="5rem"
-          color="green.400"
-        >
-          <CircularProgressLabel />
-        </CircularProgress>
-
-        <NumberInput
-          allowMouseWheel
-          size="md"
-          maxW={24}
-          min={0}
-          value={val !== null ? val : 0}
-          defaultValue={0}
-          isReadOnly={isReadOnly}
-          onChange={(value) => setVal(Number(value))}
-        >
-          <NumberInputField />
-          <NumberInputStepper className="number-input-stepper">
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-        <Button onClick={() => setCountdown()}>Submit</Button>
-      </HStack>
-    </>
-  );
-  // User sets time limit. Progress bar indicates how much time they have left. When time is up, lock the text editor. But ofc, give the user the option to unlock it if they want to.
-}
-
-//TODO: have 3 radio buttons for each "mode". prompt mode, word count mode, and time limit mode. for each click, render their respective components. ONLY ONE CAN BE RENDERED/SELECTED AT A TIME.
-
-//? PROMPTMODE COMPONENT, TIMEMODECOMPONENT, WORDCOUNTCOMPONENT
-
-function TextEditor({ quillEditorRef, postTitleRef }) {
+function TextEditor({ quillEditorRef, getWordCount, postTitleRef }) {
   return (
     <>
       <ContentEditable
@@ -184,60 +118,8 @@ function TextEditor({ quillEditorRef, postTitleRef }) {
         className="post-title"
         html="<h2>Untitled</h2>"
       />
-      <ReactQuill ref={quillEditorRef} theme="bubble" />
+      <ReactQuill onKeyUp={getWordCount} ref={quillEditorRef} theme="bubble" />
       {/* <ReactQuill ref={quillEditor} theme="bubble" /> */}
-    </>
-  );
-}
-
-function PromptMode() {
-  const [words, setWords] = useState([]);
-  const [error, setError] = useState(null);
-  const [numberOfWords, setNumberOfWords] = useState(0);
-
-  function changeNumberOfWords(e) {
-    setNumberOfWords(Number(e.target.value));
-  }
-
-  function getWords() {
-    if (numberOfWords > 10) {
-      return setError({
-        errorMessage: "Number must be below 10.",
-      });
-    } else {
-      setError(null);
-    }
-
-    fetch(`https://random-word-api.herokuapp.com/word?number=${numberOfWords}`)
-      .then((response) => response.json())
-      .then((data) => setWords(data));
-  }
-
-  useEffect(() => {
-    getWords();
-  }, []);
-
-  //! The words refresh/it refetches a new set of words when you click on Prompt Mode, then click on another mode, then click BACK on prompt mode. Maybe I shouldn't allow the user to switch between prompts. Maybe clicking on a prompt will render that component, and it makes the options of other modes disappears. OR, clicking on another mode says that "you will lose your progress if you switch modes. Are you sure you want to?", which the user must THEN click again to confirm that's what they really want.
-
-  // ^^^ basically, if a mode has already been selected, and a NEW mode is now selected, pull up an alert box for the reader to confirm they really want to leave/lose their mode progress.
-
-  return (
-    <>
-      <div className="words-list-wrapper">
-        <input
-          className="word-number-input"
-          onChange={(e) => changeNumberOfWords(e)}
-          type="number"
-        />
-        <p>words</p>
-        <ul className="words-list">
-          {words.map((word, index) => (
-            <li key={index}>{word}</li>
-          ))}
-        </ul>
-        <BiRefresh onClick={() => getWords()} className="refresh-btn" />
-        {error && <p>{error.errorMessage}</p>}
-      </div>
     </>
   );
 }
