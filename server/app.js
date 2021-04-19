11d3639c12b5ac71f8a22c6bdc5784ce7c79bc16
Ahniya Payne <ahniyap9231@gmail.com>
2021-04-18 21:40:44 -0500

    create build version of app

--- server/app.js
+++ server/app.js
@@ -37,11 +37,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.post("/login", (req, res, next) => {
   let username = req.body.username.trim();
