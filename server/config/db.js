d208f044ff53eb7ec271ac5d63a3df8dcdb896dc
A. Monae Payne <amonaepayne@gmail.com>
2021-05-13 23:14:14 -0500

    pass db credentials in options

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>
   mongoose.connect(
-    `mongodb://admin:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00.lqubo.mongodb.net:27017,cluster0-shard-00-01.lqubo.mongodb.net:27017,cluster0-shard-00-02.lqubo.mongodb.net:27017/WriteOnDB?ssl=true&replicaSet=atlas-11s4zi-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    `mongodb+srv://@cluster0.lqubo.mongodb.net/WriteOnDB?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
+      user: "admin",
+      pass: process.env.MONGO_PASSWORD,
     }
   );
 console.log("MongoDB Connected".cyan);
