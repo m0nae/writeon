c161de42ccc4e9de36cc3b8747f8708463433d56
A. Monae Payne <amonaepayne@gmail.com>
2021-05-13 22:00:31 -0500

    change db url that the app is using

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>
   mongoose.connect(
-    `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.ixq3w.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.lqubo.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
