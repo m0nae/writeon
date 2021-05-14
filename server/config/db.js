65eb409165783792370e607c4bd7f0f05b792315
A. Monae Payne <amonaepayne@gmail.com>
2021-05-13 22:27:07 -0500

    change db url for app again

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>
   mongoose.connect(
-    `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.lqubo.mongodb.net/WriteOnDB?retryWrites=true&w=majority`,
+    `mongodb://admin:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00.lqubo.mongodb.net:27017,cluster0-shard-00-01.lqubo.mongodb.net:27017,cluster0-shard-00-02.lqubo.mongodb.net:27017/WriteOnDB?ssl=true&replicaSet=atlas-11s4zi-shard-0&authSource=admin&retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
